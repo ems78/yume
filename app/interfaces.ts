f942e3c8d62225482e5554c3aa20a95b93f51b12
ems78 <cortn778xz@gmail.com>
2024-02-08 12:44:19 +0100

    define user interface and schema. rename models file from models to schemas

--- app/interfaces.ts
+++ app/interfaces.ts
@@ -8,10 +8,20 @@ interface Tag extends Document {
 
 interface DreamLog extends Document {
   _id: ObjectId;
+  userId: ObjectId;
   date: Date;
   title: string;
   content: string;
   tags: Types.ObjectId[];
+}
+
+interface User extends Document {
+  _id: ObjectId;
+  username: string;
+  passwordHash: string;
+  email: string;
+  tags: Types.ObjectId[];
+  dreamLogs: Types.ObjectId[];
 }
 
 export { Tag, DreamLog };
--- app/schemas.ts
+++ app/schemas.ts
@@ -8,10 +8,19 @@ const tagSchema = new mongoose.Schema<Tag>({
 });
 
 const dreamLogSchema = new mongoose.Schema<DreamLog>({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   date: { type: Date, required: true },
   title: { type: String, required: true },
   content: { type: String, required: true },
   tags: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tag" }],
+});
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  tags: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tag" }],
+  dreamLogs: [{ type: mongoose.Schema.Types.ObjectId, ref: "DreamLog" }],
 });
 
 export const TagModel = mongoose.model<Tag>("Tag", tagSchema);
