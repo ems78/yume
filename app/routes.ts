aa14db1b33e9579145a209597d015c65ac6d135a
ems78 <cortn778xz@gmail.com>
2024-02-13 15:56:16 +0100

    add comments to route definitions

--- app/routes.ts
+++ app/routes.ts
@@ -32,7 +32,17 @@ import { authenticateToken } from "./middleware/authenticateToken";
 
 const router = express.Router();
 
+// Dream Log routes //
+/**
+ * GET /api/logs
+ * returns all logs
+ */
 router.get("/logs", authenticateToken, getLogs);
+
+/**
+ * GET /api/logs/:id
+ * returns a log by id
+ */
 router.get(
   "/logs/:id",
   authenticateToken,
@@ -40,6 +50,11 @@ router.get(
   handleValidationResult,
   getLogById
 );
+
+/**
+ * POST /api/logs
+ * adds a log
+ */
 router.post(
   "/logs",
   authenticateToken,
@@ -47,6 +62,11 @@ router.post(
   handleValidationResult,
   addLog
 );
+
+/**
+ * PUT /api/logs/:id
+ * edits a log
+ */
 router.put(
   "/logs/:id",
   authenticateToken,
@@ -54,6 +74,11 @@ router.put(
   handleValidationResult,
   editLog
 );
+
+/**
+ * DELETE /api/logs/:id
+ * deletes a log
+ */
 router.delete(
   "/logs/:id",
   authenticateToken,
@@ -62,20 +87,74 @@ router.delete(
   deleteLog
 );
 
+// ------------------------------------------------- //
+// Tag routes //
+/**
+ * GET /api/tags
+ * returns all tags
+ */
 router.get("/tags", authenticateToken, getTags);
-router.get("/tags/:id", authenticateToken, validateId(), handleValidationResult, getTagById);
+
+/**
+ * GET /api/tags/:id
+ * returns a tag by id
+ */
+router.get(
+  "/tags/:id",
+  authenticateToken,
+  validateId(),
+  handleValidationResult,
+  getTagById
+);
+
+/**
+ * POST /api/tags
+ * adds a tag
+ */
 router.post("/tags", tagValidationRules(), handleValidationResult, addTag);
-router.put("/tags/:id", authenticateToken, validateId(), handleValidationResult, editTag);
+router.put(
+  "/tags/:id",
+  authenticateToken,
+  validateId(),
+  handleValidationResult,
+  editTag
+);
+
+/**
+ * DELETE /api/tags/:id
+ * deletes a tag
+ */
 router.delete("/tags/:id", validateId(), handleValidationResult, deleteTag);
 
+// ------------------------------------------------- //
+// User routes //
+/**
+ * POST /api/login
+ * logs in a user
+ */
 router.post("/login", loginValidationRules(), handleValidationResult, login);
+
+/**
+ * GET /api/account
+ * returns user info
+ */
 router.get("/account", authenticateToken, getUserInfo);
+
+/**
+ * POST /api/register
+ * registers a user
+ */
 router.post(
   "/register",
   registrationValidationRules(),
   handleValidationResult,
   registerUser
 );
+
+/**
+ * PUT /api/account
+ * edits user info
+ */
 router.put(
   "/account",
   editAccountInfoValidationRules(),
